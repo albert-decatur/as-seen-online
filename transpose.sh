0c40d16281c2501fe05f79bc4404f5efdefcf21c
Albert Decatur <adecatur@aiddata.org>
2014-10-22 17:57:29 -0400

    transpose TSV

--- transpose.sh
+++ transpose.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# written with gawk - appears to work with awk
+# originally written with gawk - appears to work with awk
+# smal edits to make it use TSV as input and output
 
-awk '
+awk -F '\t' '
 { 
     for (i=1; i<=NF; i++)  {
         a[NR,i] = $i
@@ -12,7 +13,7 @@ END {
     for(j=1; j<=p; j++) {
         str=a[1,j]
         for(i=2; i<=NR; i++){
-            str=str" "a[i,j];
+            str=str"\t"a[i,j];
         }
         print str
     }
